edff6949c9d28729de6868e18d6eab896f5e33b1
gongtong <gongtong@2dfire.com>
2017-10-19 00:06:19 +0800

    init

---
+++ ActivityModule/src/main/java/com/person/gongtong/activitymodule/ActInit.java
@@ -0,0 +1,11 @@
+package com.person.gongtong.activitymodule;
+
+/***********************************************
+ * <P> desc:
+ * <P> Author: gongtong
+ * <P> Date: 2017/10/18 18:34
+ * <P> Copyright  2008 二维火科技
+ ***********************************************/
+
+public class ActInit {
+}
